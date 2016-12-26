471f983be801915f7c2e64dfb3b9f5c9fefb963e
韩云强 <hanyunqiang@hanyunqiangdeMacBook-Pro.local>
2016-12-27 00:09:45 +0800

    更新game_round接口，添加 Log tag 标识

--- Server/skynet/gameserver/logic/gameRound.lua
+++ Server/skynet/gameserver/logic/gameRound.lua
@@ -1,1 +1,68 @@
 --gameRound
+--单局的状态管理 开始游戏,洗牌,发牌,操作处理..定地主.
+
+--require
+
+
+local game_round = class();	
+
+--ctor
+function game_round.ctor()
+	-- body
+	Log.i(Log.tag.game_round, "[game_round]:ctor")
+
+	game_round.init(self);
+end
+
+--ctor
+function game_round.dtor()
+	-- body
+	Log.i(Log.tag.game_round, "[game_round]:dtor")
+end
+
+--init
+function game_round.init()
+	-- body
+	Log.i(Log.tag.game_round, "[game_round]:init")
+	self.m_players = {};
+
+end
+
+--洗牌
+function game_round.shuffle()
+	-- body
+	Log.i(Log.tag.game_round, "[game_round]:shuffle")
+end
+
+--发牌
+function game_round.deal_poker()
+	-- body
+	Log.i(Log.tag.game_round, "[game_round]:shuffle")
+end
+
+--操作
+function game_round.operate()
+	-- body
+	Log.i(Log.tag.game_round, "[game_round]:operate")
+end
+
+--选地主
+function game_round.select_landlord()
+	-- body
+	Log.i(Log.tag.game_round, "[game_round]:select_landlord")
+end
+
+--玩家进入
+function game_round.player_enter(  )
+	-- body
+	Log.i(Log.tag.game_round, "[game_round]:player_enter")
+end
+
+--玩家进入
+function game_round.player_exit(  )
+	-- body
+	Log.i(Log.tag.game_round, "[game_round]:player_enter")
+end
+
+
+
